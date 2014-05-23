a14f78cca764deb0ed39c23a440dd71bb76dd972
SANDY SANTOS <sandysantos@mycrosan.com.br>
2014-05-23 01:40:56 -0300

    versão 1.6
    Tarley e Leandro no projeto

--- classes/retornaCliente.php
+++ classes/retornaCliente.php
@@ -7,20 +7,11 @@ $db['pass'] = "";
 mysql_connect($db['host'], $db['user'], $db['pass']);
 mysql_select_db($db['nome']);
 $req = "SELECT * FROM clientes WHERE TELEFONE LIKE '%".$_REQUEST['term']."%'";
-<<<<<<< HEAD
-$query = mysql_query($req) or die(mysql_error());
-$query = mysql_query($req) or die(mysql_error());
-=======
-<<<<<<< HEAD
-$query = mysql_query($req) or die(mysql_error());
-=======
 
 $query = mysql_query($req) or die(mysql_error());
-
-
-
->>>>>>> 328c4af7d7721ba7dc461bfb256697ceb9cfa7f1
->>>>>>> 70962c4b8e8d7a30b06035e26dd24a23f7c38ee9
+$query = mysql_query($req) or die(mysql_error());
+$query = mysql_query($req) or die(mysql_error());
+$query = mysql_query($req) or die(mysql_error());
 while($row = mysql_fetch_array($query))
 {
   $results[] = array(
